03fe39df00762f9d3fcae61f0b050de0b566671c
muxx3 <186375124+muxx3@users.noreply.github.com>
2025-07-31 22:00:44 -0700

    Adjust configs

--- fish/.config/fish/conf.d/bobthefish.fish
+++ fish/.config/fish/conf.d/bobthefish.fish
@@ -1,5 +1,5 @@
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme terminal
+set -g theme_color_scheme zenburn
 set -g theme_display_user ssh
 set -g default_user muxee
 set -g theme_display_node yes
