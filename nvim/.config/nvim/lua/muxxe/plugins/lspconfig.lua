b9d0937850b8ecfb5ad3d44a6b8b43a7f02e2719
muxx3 <186375124+muxx3@users.noreply.github.com>
2025-09-24 00:27:58 -0700

    (fix): remove custom servers to avoid error for v3.0 lsp_server migration

--- nvim/.config/nvim/lua/muxxe/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/muxxe/plugins/lspconfig.lua
@@ -3,28 +3,10 @@ local lspconfig = require("lspconfig")
 local caps = require("cmp_nvim_lsp").default_capabilities()
 
 local servers = {
-  "html", "cssls", "ts_ls", "eslint",
-  "taplo", "bashls", "lua_ls",
-  "jsonls", "yamlls", "dockerls", "marksman",
+    "html", "cssls", "ts_ls", "eslint",
+    "taplo", "bashls", "lua_ls",
+    "jsonls", "yamlls", "dockerls", "marksman", "pylsp",
 }
 
-for _, server in ipairs(servers) do
-  local opts = { capabilities = caps }
 
-  if server == "lua_ls" then
-    opts.settings = {
-      Lua = {
-        runtime = { version = "LuaJIT" },
-        diagnostics = { globals = { "vim" } },
-        workspace = {
-          library = vim.api.nvim_get_runtime_file("", true),
-          checkThirdParty = false,
-        },
-        telemetry = { enable = false },
-      }
-    }
-  end
-
-  lspconfig[server].setup(opts)
-end
 
